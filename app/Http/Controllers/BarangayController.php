63d433eb8746d7866e6862de3390a057916e8ceb
John Paul <jpgulayan@gmail.com>
2020-06-13 10:49:26 +0800

    add barangay psgc

--- app/Http/Controllers/BarangayController.php
+++ app/Http/Controllers/BarangayController.php
@@ -36,7 +36,7 @@ class BarangayController extends Controller
     }
     public function listBarangays($province_psgc, $city_psgc)
     {
-        $query = Barangay::select('barangay_name','id');
+        $query = Barangay::select('barangay_name','id','barangay_psgc');
         $query->distinct();
         $query->where('province_psgc', $province_psgc);
         $query->where('city_psgc', $city_psgc);
